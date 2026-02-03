f9a3621f285497e3cb89f51a720a6ded0fd93ed8
s-mgy <maggyandriambelomahery@gmail.com>
2026-02-03 16:27:46 +0100

    [test:]first scenario

--- src/webinars/use-cases/change-seats.test.ts
+++ src/webinars/use-cases/change-seats.test.ts
@@ -1,13 +1,42 @@
+import { InMemoryWebinarRepository } from 'src/webinars/adapters/webinar-repository.in-memory';
+import { ChangeSeats } from './change-seats';
+import { Webinar } from 'src/webinars/entities/webinar.entity';
+import { testUser } from 'src/users/tests/user-seeds';
+
 // Tests unitaires
-
-
 describe('Feature : Change seats', () => {
   // Initialisation de nos tests, boilerplates...
   describe('Scenario: Happy path', () => {
     // Code commun à notre scénario : payload...
+    const payload = {
+      user: testUser.alice,
+      webinarId: 'webinar-id',
+      seats: 200,
+    };
+
+    let webinarRepository: InMemoryWebinarRepository;
+    let useCase: ChangeSeats;
+
+    const webinar = new Webinar({
+      id: 'webinar-id',
+      organizerId: testUser.alice.props.id,
+      title: 'Webinar title',
+      startDate: new Date('2024-01-01T00:00:00Z'),
+      endDate: new Date('2024-01-01T01:00:00Z'),
+      seats: 100,
+    });
+
+    beforeEach(() => {
+      webinarRepository = new InMemoryWebinarRepository([webinar]);
+      useCase = new ChangeSeats(webinarRepository);
+    });
+
     it('should change the number of seats for a webinar', async () => {
-     // Vérification de la règle métier, condition testée...
-      throw new Error('Not implemented');
+      // ACT
+      await useCase.execute(payload);
+      // ASSERT
+      const updatedWebinar = await webinarRepository.findById('webinar-id');
+      expect(updatedWebinar?.props.seats).toEqual(200);
     });
   });
 });
